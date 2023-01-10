c4b98c77c0c69f0ee198bd413a4f2b1655570827
lokuandrewso <lokuandrewso@gmail.com>
2023-01-10 15:05:35 +0000

    first commit

---
+++ tire_volume.py
@@ -0,0 +1,19 @@
+# Import the math module so that I can use math.pi
+import math
+
+# Print a description of this program for the user to see
+print("This program computes and outputs the volume of a car tire")
+
+# Get the width in millimeters, aspect ratio and diameter of the tire in inches from the user.
+width = float(input("Enter the width of the tire in mm: "))
+asp_ratio = float(input("Enter the aspect ratio of the tire: "))
+diameter = float(input("Enter the diameter of the wheel in inches: "))
+
+# Compute the volume of the tire.
+volume = (math.pi * width**2 * asp_ratio * (width * asp_ratio + 2540 * diameter)) / 10000000000
+
+# Round the volume to two digits after the decimal point.
+volume = round(volume, 2)
+
+# Print the results for the user to see.
+print(f"The approximate volume is {volume} liters")

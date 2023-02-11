fc08ed20afb42c9faaa4967be8f66f31d47f7d83
lokuandrewso <lokuandrewso@gmail.com>
2023-02-11 13:55:24 +0000

    made edits to the program

--- Practice/sentences.py
+++ Practice/sentences.py
@@ -1,5 +1,55 @@
 import random
 
+def main():
+
+    a = get_determiner(1)
+    b = get_noun(7)
+    c = get_verb(1, "past")
+    d = get_preposition(1)
+    e = get_prepositional_phrase(6)
+    f = get_prepositional_phrase(2)
+    print (f"{a} {b} {c} {d} {f} {e}")
+
+    a2 = get_determiner(1)
+    b2 = get_noun(4)
+    c2 = get_verb(1, "present")
+    d2 = get_preposition(4)
+    e2 = get_prepositional_phrase(3)
+    f2 = get_prepositional_phrase(1)
+    print (f"{a2} {b2} {c2} {f2} {d2} {e2}")
+
+    a3 = get_determiner(2)
+    b3 = get_noun(3)
+    c3 = get_verb(4, "future")
+    d3 = get_preposition(3)
+    e3 = get_prepositional_phrase(4)
+    f3 = get_prepositional_phrase(2)
+    print (f"{a3} {b3} {c3} {d3} {f3} {e3}")
+
+    a4 = get_determiner(2)
+    b4 = get_noun(3)
+    c4 = get_verb(2, "past")
+    d4 = get_preposition(4)
+    e4 = get_prepositional_phrase(2)
+    f4 = get_prepositional_phrase(3)
+    print (f"{a4} {b4} {c4} {d4} {e4} {f4}")
+
+    a5 = get_determiner(2)
+    b5 = get_noun(2)
+    c5 = get_verb(2, "present")
+    d5 = get_preposition(2)
+    e5 = get_prepositional_phrase(2)
+    f5 = get_prepositional_phrase(1)
+    print (f"{a5} {b5} {f5} {c5} {d5} {e5}")
+
+    a6 = get_determiner(2)
+    b6 = get_noun(2)
+    c6 = get_verb(4, "future")
+    d6 = get_preposition(2)
+    e6 = get_prepositional_phrase(2)
+    f6 = get_prepositional_phrase(1)
+    print (f"{a6} {b6} {c6} {d6} {f6} {e6}")
+
 def get_determiner(quantity):
     """Return a randomly chosen determiner. A determiner is
     a word like "the", "a", "one", "some", "many".
@@ -41,13 +91,13 @@ def get_noun(quantity):
     Return: a randomly chosen noun.
     """
     if quantity == 1:
-        nouns = ["bird", "boy", "car", "cat", "child", "dog", "girl", "man", "rabbit", "woman"]
+        words = ["bird", "boy", "car", "cat", "child", "dog", "girl", "man", "rabbit", "woman"]
     else:
-        nouns = ["birds", "boys", "cars", "cats", "children", "dogs", "girls", "men", "rabbits", "women"]
+        words = ["birds", "boys", "cars", "cats", "children", "dogs", "girls", "men", "rabbits", "women"]
 
-    # Randomly return a chosen noun
-    noun = random.nouns(nouns)
-    return noun
+    # Randomly return a chosen noun.
+    word = random.choice(words)
+    return word
 
 def get_verb(quantity, tense):
     """Return a randomly chosen verb. If tense is "past",
@@ -76,16 +126,72 @@ def get_verb(quantity, tense):
     Return: a randomly chosen verb.
     """
     if tense == "past":
-        verbs = ["drank", "ate", "grew", "laughed", "thought","ran", "slept", "talked", "walked", "wrote"]
-    elif quantity == 1:
-        verbs = ["drinks", "eats", "grows", "laughs", "thinks", "runs", "sleeps", "talks", "walks", "writes"]
-    elif quantity != 1:
-        verbs = ["drink", "eat", "grow", "laugh", "think", "run", "sleep", "talk", "walk", "write"]
-    elif tense == "future":
-        verbs = [ "will drink", "will eat", "will grow", "will laugh", "will think", "will run", "will sleep", "will talk",
+        words = ["drank", "ate", "grew", "laughed", "thought","ran", "slept", "talked", "walked", "wrote"]
+
+    elif tense == "present" and quantity == 1:
+        words = ["drinks", "eats", "grows", "laughs", "thinks", "runs", "sleeps", "talks", "walks", "writes"]
+
+    elif tense == "present" and quantity != 1:
+        words = ["drink", "eat", "grow", "laugh", "think", "run", "sleep", "talk", "walk", "write"]
+
+    else:
+        words = [ "will drink", "will eat", "will grow", "will laugh", "will think", "will run", "will sleep", "will talk",
         "will walk", "will write"]
     
     #Randomly return a chosen verb
-    verb = random.verbs(verbs)
-    return verb 
+    word = random.choice(words)
+    return word 
+
+def get_preposition(quantity):
+    """Return a randomly chosen preposition
+    from this list of prepositions:
+        "about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"
+
+    Return: a randomly chosen preposition.
+    """
+    words = ["about", "above", "across", "after", "along", "around", "at", "before", "behind", "below", "beyond", "by", "despite", "except", "for", "from", "in", "into", "near", "of", "off", "on", "onto", "out", "over", "past", "to", "under", "with", "without"]
+    word = random.choice(words)
+    return word
+
+def get_prepositional_phrase(quantity):
+    """Build and return a prepositional phrase composed
+    of three words: a preposition, a determiner, and a
+    noun by calling the get_preposition, get_determiner,
+    and get_noun functions.
+
+    Parameter
+        quantity: an integer that determines if the
+            determiner and noun in the prepositional
+            phrase returned from this function should
+            be single or pluaral.
+    Return: a prepositional phrase.
+    """
+    words = ["about", "above", "across", "after", "along", "around", "at", "before", "behind", "below", "beyond", "by", "despite", "except", "for", "from", "in", "into", "near", "of", "off", "on", "onto", "out", "over", "past", "to", "under", "with", "without"]
+
+    if quantity == 1:
+        words = ["a", "one", "the"]
+    else:
+        words = ["some", "many", "the"]
+
+    if quantity == 1:
+        words = ["bird", "boy", "car", "cat", "child", "dog", "girl", "man", "rabbit", "woman"]
+    else:
+        words = ["birds", "boys", "cars", "cats", "children", "dogs", "girls", "men", "rabbits", "women"]
+
+    # Randomly return a prepositional phrase.
+    word = random.choice(words)
+    return word
+
+# If this file is executed like this:
+# > python program.py
+# then call the main function. However, if this file is simply
+# imported (e.g. into a test file), then skip the call to main.
+if __name__ == "__main__":
+    main()
+
 
--- Practice/test_sentences.py
+++ Practice/test_sentences.py
@@ -1,4 +1,4 @@
-from sentences import get_determiner, get_noun, get_verb
+from sentences import get_determiner, get_noun, get_verb, get_preposition, get_prepositional_phrase
 import random
 import pytest
 
@@ -36,6 +36,63 @@ def test_get_determiner():
         # Verify that the word returned from get_determiner
         # is one of the words in the plural_determiners list.
         assert word in plural_determiners
+
+def test_get_noun():
+    #get the single nouns
+    single_nouns = ["bird", "boy", "car", "cat", "child", "dog", "girl", "man", "rabbit", "woman"]
+    for _ in range(11):
+        word = get_noun(1)
+        assert word in single_nouns
+
+
+    #Get the plural nouns
+    plural_nouns = ["birds", "boys", "cars", "cats", "children", "dogs", "girls", "men", "rabbits", "women"]
+    for _ in range(11):
+        word = get_noun(2)
+        assert word in plural_nouns
+
+def test_get_verb():
+    #Call the get_verb function to assert the statement
+    assert get_verb(1, "future") != "drinks"
+    assert get_verb(2, "past") != "thinks"
+
+
+def test_get_preposition():
+    #get the prepositional words.
+    prepo_words = ["about", "above", "across", "after", "along", "around", "at", "before", "behind", "below", "beyond", "by", "despite", "except", "for", "from", "in", "into", "near", "of", "off", "on", "onto", "out", "over", "past", "to", "under", "with", "without"]
+    for _ in range(30):
+        word = get_preposition(5)
+        assert word in prepo_words
+
+def test_get_prepositional_phrase():
+    #get the prepostions
+    prepo_words = ["about", "above", "across", "after", "along", "around", "at", "before", "behind", "below", "beyond", "by", "despite", "except", "for", "from", "in", "into", "near", "of", "off", "on", "onto", "out", "over", "past", "to", "under", "with", "without"]
+    for _ in range(30):
+        word = get_preposition(5)
+        assert word in prepo_words
+
+    #Get the determiners
+    single_determiners = ["a", "one", "the"]
+    for _ in range(4):
+        word = get_determiner(1)
+        assert word in single_determiners
+
+    plural_determiners = ["some", "many", "the"]
+    for _ in range(4):
+        word = get_determiner(2)
+        assert word in plural_determiners
+
+    #Get the nouns
+    single_nouns = ["bird", "boy", "car", "cat", "child", "dog", "girl", "man", "rabbit", "woman"]
+    for _ in range(11):
+        word = get_noun(1)
+        assert word in single_nouns
+
+    plural_nouns = ["birds", "boys", "cars", "cats", "children", "dogs", "girls", "men", "rabbits", "women"]
+    for _ in range(11):
+        word = get_noun(2)
+        assert word in plural_nouns
+
 
 # Call the main function that is part of pytest so that the
 # computer will execute the test functions in this file.
